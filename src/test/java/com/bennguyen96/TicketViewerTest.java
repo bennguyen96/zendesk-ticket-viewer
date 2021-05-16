7be386fafe56fc6d07351e5d2d8f0bca813f3215
Ben <bennguyen96@gmail.com>
2021-05-16 21:20:42 +1000

    add happy path tests

--- src/test/java/com/bennguyen96/TicketViewerTest.java
+++ src/test/java/com/bennguyen96/TicketViewerTest.java
@@ -12,34 +12,17 @@ import static org.junit.Assert.assertEquals;
 
 public class TicketViewerTest {
 
-    private TicketViewer ticketViewer;
-    private ByteArrayOutputStream out = new ByteArrayOutputStream();
-    private PrintStream originalOut = System.out;
-    private InputStream sysInBackup;
-
-    @Before
-    public void setUp()  {
-        ticketViewer = new com.bennguyen96.TicketViewer();
-        System.setOut(new PrintStream(out));
-        sysInBackup = System.in;
-    }
-
-    @After
-    public void cleanUp() {
-        System.setOut(originalOut);
-        System.setIn(sysInBackup);
+    @Test
+    public void happyPathAllTicketsTest() throws Throwable {
+        ByteArrayInputStream in = new ByteArrayInputStream("1\nnext\nquit".getBytes());
+        System.setIn(in);
+        TicketViewer.main(null);
     }
 
     @Test
-    public void happyPathTest() throws Throwable {
-        TicketViewer.main(null);
-        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+    public void happyPathOneTicketTest() throws Throwable {
+        ByteArrayInputStream in = new ByteArrayInputStream("2\n1\nquit".getBytes());
         System.setIn(in);
-
-        assertEquals("Welcome to the Zendesk Ticket Viewer\n" +
-                "Select '1' to view all tickets\n" +
-                "Select '2' to view a ticket\n" +
-                "Type 'quit' to exit", out.toString());
-
+        TicketViewer.main(null);
     }
 }
